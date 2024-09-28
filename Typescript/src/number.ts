032064819906be7fb732c9de97b31dc0e42aaf8b
aziz <siliconoxide4@gmail.com>
2024-09-28 14:07:48 +0600

    added typescript

---
+++ Typescript/src/number.ts
@@ -0,0 +1,18 @@
+let price: number;
+price = 100;
+console.log(price); // 100
+price = 10.9;
+console.log(price);
+price = 0b1010; //binary
+console.log(price);
+price = 0o777; //octal
+console.log(price);
+price = 0x123; //hexadecimal
+console.log(price);
+
+let intialnum: number[] = [];
+intialnum.push(1);
+console.log(intialnum);
+
+let list: [number, number, number];
+list = [1, 2, 3];
