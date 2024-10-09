a0469ab2658074e61ab70842087f70c922377f12
aziz <siliconoxide4@gmail.com>
2024-10-09 18:23:39 +0600

    add oop

--- dart/src/checkClass.dart
+++ dart/src/checkClass.dart
@@ -1,12 +1,1 @@
-import 'class.dart';
 
-void main(List<String> args) {
-  List<Person> persons = [
-    Person("Aziz", 25),
-    Person("Rahman", 24),
-    Person("Rahim", 23),
-  ];
-  print(persons[2]);
-  var person = Person("Aziz", 25);
-  print(person.name);
-}
---
+++ dart/src/class/class.dart
@@ -0,0 +1,8 @@
+import 'circle.dart';
+
+void main(List<String> args) {
+  Circle circle = Circle(10);
+  print(circle.area());
+  Square sq = Square(10);
+  print(sq.area());
+}
